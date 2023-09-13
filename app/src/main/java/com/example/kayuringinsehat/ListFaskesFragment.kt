2895f7a58f762cff1fa1076203c3a7d186c605fc
syahdan gumilang <syahdangumilang.sg@gmail.com>
2023-09-13 13:19:22 +0700

    Perbaikan kode xml tampilan layanan | 13:19 - 13 sept 2023

--- app/src/main/java/com/example/kayuringinsehat/ListFaskesFragment.kt
+++ app/src/main/java/com/example/kayuringinsehat/ListFaskesFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [BlankFragment2.newInstance] factory method to
+ * Use the [ListFaskesFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class BlankFragment2 : Fragment() {
+class ListFaskesFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -49,7 +49,7 @@ class BlankFragment2 : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            BlankFragment2().apply {
+            ListFaskesFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
