b82bd81f096b219ddf4bbe2d42f03fe8ac17b8d5
Aji Saputra <mhmdajisaputraa@gmail.com>
2023-09-18 00:35:25 +0700

    perubahan sementara di kt bmi, penambahan xml gizi | 23:43 17 sept 2023

--- app/src/main/java/com/example/kayuringinsehat/BmiActivity.kt
+++ app/src/main/java/com/example/kayuringinsehat/BmiActivity.kt
@@ -2,17 +2,50 @@ package com.example.kayuringinsehat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.kayuringinsehat.databinding.ActivityBmiBinding
-import java.text.NumberFormat
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+
 
 class BmiActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityBmiBinding
+//    private lateinit var binding: ActivityBmiBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityBmiBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_bmi)
+//        binding = ActivityBmiBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+
+        val berBadanText = findViewById<EditText>(R.id.berat_badan)
+        val tingBadanText = findViewById<EditText>(R.id.tinggi_badan)
+        val tombolHitung = findViewById<Button>(R.id.btn_hitung_bmi)
+
+        tombolHitung.setOnClickListener {
+            val berat = berBadanText.text.toString()
+            val tinggi = tingBadanText.text.toString()
+
+            val bmi = berat.toFloat()/((tinggi.toFloat()/100)*(tinggi.toFloat()/100))
+            // get result with two decimal place
+            val bmi2digits = String.format("%.2f",bmi).toFloat()
+            hasilTampilan(bmi2digits)
+        }
+    }
+
+    private fun hasilTampilan(bmi:Float){
+        val gambarBMI = findViewById<ImageView>(R.id.iv_status_obesitas)
+        val hasilBMI = findViewById<TextView>(R.id.tv_status_obesitas)
+
+        hasilBMI.text = bmi.toString()
+
+        when {
+            bmi < 18.5 -> gambarBMI.setImageResource(R.drawable.underweight)
+            bmi in 18.5..24.9 -> gambarBMI.setImageResource(R.drawable.normal)
+            bmi in 25.0..29.9 -> gambarBMI.setImageResource(R.drawable.overweight)
+            bmi > 30 -> gambarBMI.setImageResource(R.drawable.obesity)
+        }
+    }
     }
 
 //        binding.btnHitungBmi.setOnClickListener {
@@ -45,8 +78,3 @@ class BmiActivity : AppCompatActivity() {
 //        }
 //        NumberFormat.getCurrencyInstance()
 //        displayTip(tip)
-
-
-
-
-}
