a01d89e95e73b5ae1d0afd82bc1280d251e64bcb
PC 15 - 14 <https://github.com/AjiSaputra07/latihanRoomDB.git>
2023-09-18 10:40:59 +0700

    Menyelesaikan xml, kt bmi, dan penambahan warna | 10:40 - 18 Sept 2023

--- app/src/main/java/com/example/kayuringinsehat/BmiActivity.kt
+++ app/src/main/java/com/example/kayuringinsehat/BmiActivity.kt
@@ -6,17 +6,15 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 
 
 class BmiActivity : AppCompatActivity() {
 
-//    private lateinit var binding: ActivityBmiBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bmi)
-//        binding = ActivityBmiBinding.inflate(layoutInflater)
-//        setContentView(binding.root)
 
         val berBadanText = findViewById<EditText>(R.id.berat_badan)
         val tingBadanText = findViewById<EditText>(R.id.tinggi_badan)
@@ -25,56 +23,60 @@ class BmiActivity : AppCompatActivity() {
         tombolHitung.setOnClickListener {
             val berat = berBadanText.text.toString()
             val tinggi = tingBadanText.text.toString()
-
-            val bmi = berat.toFloat()/((tinggi.toFloat()/100)*(tinggi.toFloat()/100))
-            // get result with two decimal place
-            val bmi2digits = String.format("%.2f",bmi).toFloat()
-            hasilTampilan(bmi2digits)
+            if (validateInput(berat, tinggi)) {
+                val bmi = berat.toFloat() / ((tinggi.toFloat() / 100) * (tinggi.toFloat() / 100))
+                // get result with two decimal place
+                val bmi2digits = String.format("%.2f", bmi).toFloat()
+                hasilTampilan(bmi2digits)
+            }
         }
     }
 
-    private fun hasilTampilan(bmi:Float){
+    private fun validateInput(berat: String?, tinggi: String?): Boolean {
+        return when {
+            berat.isNullOrEmpty() -> {
+                Toast.makeText(this, "Berat Badan Tidak Boleh Kosong", Toast.LENGTH_SHORT).show()
+                return false
+            }
+            tinggi.isNullOrEmpty() -> {
+                Toast.makeText(this, "Tinggi Badan Tidak Boleh Kosong", Toast.LENGTH_SHORT).show()
+                return false
+            }
+
+            else -> {
+                return true
+            }
+        }
+    }
+
+    private fun hasilTampilan(bmi: Float) {
         val gambarBMI = findViewById<ImageView>(R.id.iv_status_obesitas)
         val hasilBMI = findViewById<TextView>(R.id.tv_status_obesitas)
 
         hasilBMI.text = bmi.toString()
+        var color = 0
 
         when {
-            bmi < 18.5 -> gambarBMI.setImageResource(R.drawable.underweight)
-            bmi in 18.5..24.9 -> gambarBMI.setImageResource(R.drawable.normal)
-            bmi in 25.0..29.9 -> gambarBMI.setImageResource(R.drawable.overweight)
-            bmi > 30 -> gambarBMI.setImageResource(R.drawable.obesity)
+            bmi < 18.5 -> {
+                gambarBMI.setImageResource(R.drawable.underweight)
+                color = R.color.biruBMICharacter
+            }
+
+            bmi in 18.5..24.9 -> {
+                gambarBMI.setImageResource(R.drawable.normal)
+                color = R.color.hijauBMIcharacter
+            }
+
+            bmi in 25.0..29.9 -> {
+                gambarBMI.setImageResource(R.drawable.overweight)
+                color = R.color.orangeBMICharacter
+            }
+
+            bmi > 30 -> {
+                gambarBMI.setImageResource(R.drawable.obesity)
+                color = R.color.merahBMICharacter
+            }
         }
+        hasilBMI.setTextColor(ContextCompat.getColor(this, color))
     }
-    }
-
-//        binding.btnHitungBmi.setOnClickListener {
-//            hitungBMI()
-//        }
-//    }
-
-//    private fun hitungBMI() {
-//
-//        val stringInputBerat = binding.beratBadan.text.toString()
-//        val beratBmi = stringInputBerat.toDoubleOrNull()
-//        if (beratBmi == )
-//
-//    }
-
-//        val stringInTextField = binding.costOfServiceEditText.text.toString()
-//        val cost = stringInTextField.toDoubleOrNull()
-//        if (cost == null || cost == 0.0) {
-//            displayTip(0.0)
-//            return
-//        }
-//        val tipPersentage = when (binding.tipOptions.checkedRadioButtonId) {
-//            R.id.option_fifteen_percent -> 0.15
-//            R.id.option_eighteen_percent -> 0.18
-//            else -> 0.20
-//        }
-//        var tip = tipPersentage * cost
-//        if (binding.roundUpSwitch.isChecked) {
-//            tip = kotlin.math.ceil(tip)
-//        }
-//        NumberFormat.getCurrencyInstance()
-//        displayTip(tip)
+}
