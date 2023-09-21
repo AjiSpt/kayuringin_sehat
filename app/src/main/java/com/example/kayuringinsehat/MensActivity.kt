8446d80c2dcb493cfb14348612ac744e416eab46
PC 15 - 14 <https://github.com/AjiSaputra07/latihanRoomDB.git>
2023-09-21 15:12:26 +0700

    Perubahan bmi finsih udah fix, mens xml proses | 15:12 - 21 Sept 2023

--- app/src/main/java/com/example/kayuringinsehat/MensActivity.kt
+++ app/src/main/java/com/example/kayuringinsehat/MensActivity.kt
@@ -4,7 +4,6 @@ import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
-import android.widget.EditText
 import com.example.kayuringinsehat.databinding.ActivityMensBinding
 import java.time.LocalDate
 import java.util.Calendar
@@ -12,10 +11,10 @@ import java.util.Calendar
 class MensActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMensBinding
-    lateinit var dateEdt: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMensBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -23,9 +22,7 @@ class MensActivity : AppCompatActivity() {
         val adapter = ArrayAdapter(this, R.layout.list_mens, number)
         binding.atDropDown.setAdapter(adapter)
 
-        dateEdt = findViewById(R.id.hari_pertama)
-
-        dateEdt.setOnClickListener {
+        binding.hariPertama.setOnClickListener {
             val c = Calendar.getInstance()
             val year = c.get(Calendar.YEAR)
             val month = c.get(Calendar.MONTH)
@@ -33,29 +30,28 @@ class MensActivity : AppCompatActivity() {
             val datePickerDialog = DatePickerDialog(
                 this,
                 { view, selectedYear, monthOfYear, dayOfMonth ->
-                    val dateStr = String.format("%02d-%02d-%d", dayOfMonth, monthOfYear + 1, selectedYear)
-                    dateEdt.setText(dateStr)
+                    val dateStr =
+                        String.format("%02d-%02d-%d", dayOfMonth, monthOfYear + 1, selectedYear)
+                    binding.hariPertama.setText(dateStr)
                 }, year, month, day
             )
             datePickerDialog.datePicker.maxDate = System.currentTimeMillis()
             datePickerDialog.show()
         }
-    }
-    fun hitungHaid(siklusHaid: Int, tanggalHariPertamaHaid: LocalDate): String {
-        if (siklusHaid < 21 || siklusHaid > 35){
+        binding.btnHitungMens.setOnClickListener {
+            val tglHariAwalHaid = binding.hariPertama.text.toString()
+            val siklusHaid = binding.atDropDown.text.toString()
 
+            if (tglHariAwalHaid != null && siklusHaid.isNotEmpty()){
+                val localDate = LocalDate.parse(tglHariAwalHaid)
+                val hasil = hitungHaid()
+            }
         }
+    }
+}
 
-        val lamaHaid = 5
-        val akhirHaid = tanggalHariPertamaHaid.plusDays((lamaHaid - 1).toLong())
-
-        val awalMasaSubur = tanggalHariPertamaHaid.plusDays(10)
-        val akhirMasaSubur = tanggalHariPertamaHaid.plusDays(17)
-
-        val hariOvulasi = tanggalHariPertamaHaid.plusDays((siklusHaid - 14).toLong())
-
-        val awalSiklusSelanjutnya = tanggalHariPertamaHaid.plusDays(siklusHaid.toLong())
-
+fun hitungHaid(siklusHaid: Int, tglHariAwalHaid: LocalDate): String{
+    if (siklusHaid < 21 || siklusHaid > 35) {
 
     }
 }
